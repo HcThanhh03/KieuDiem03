b34e3ab5bd30f90984a51bd334838d3fa2477f83
Hoàng Chí Thanh <142982456+HcThanhh03@users.noreply.github.com>
2023-09-29 23:46:11 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Kiều Diễm';
+const giftUrl = 'http://nodemy.vn';
+const eventName = '';
+const titleCard = 'Bé iu';
+const contentCard = 'Hé lô bé iu của anh, nay Trung Thu thật sự anh cũng rất muốn đi chơi với em. Chỉ cần gặp em là đủ ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'KD01.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
