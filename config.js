e16da307de07e829017199dd626160910b093063
Hoàng Chí Thanh <142982456+HcThanhh03@users.noreply.github.com>
2023-09-29 23:58:40 +0700

    update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Kiều Diễm';
+const nameGirl = 'Kiều  Diễm';
 const giftUrl = 'http://nodemy.vn';
-const eventName = '';
-const titleCard = 'Bé iu';
-const contentCard = 'Hé lô bé iu của anh, nay Trung Thu thật sự anh cũng rất muốn đi chơi với em. Chỉ cần gặp em là đủ ';
+const eventName = 'Bé iu';
+const titleCard = 'Bé iu của anh';
+const contentCard = 'Hé lô bé iu của anh, nay Trung Thu anh cũng rất muốn được chở em đi chơi. Thấy người ta đi chơi anh hong buồn đâu, chỉ buồn là anh để cho em 1 mình thuii. Anh muốn được gặp em lắm luôn, Nhớ quá chòi nhớ. Hi vọng là thuận lợi để anh được gặp em, còn không thuận lợi thì trễ 1 chút cũng được hihi ^.^ . Yêu bé nhiều';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
